bbf5f220ac3926f0eaccf7eeea24b46a842add2a
dishanth916 <rajpurohitdishant@gmail.com>
2021-08-14 15:48:00 +0530

    bugs fixed

--- sketch.js
+++ sketch.js
@@ -83,7 +83,7 @@ function draw(){
 function mouseDragged(){
     //if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+   // }
 }
 
 
@@ -95,6 +95,9 @@ function mouseReleased(){
 function keyPressed(){
     if(keyCode === 32){
        slingshot.attach(bird.body);
+       bird.trajectory=[];
+       Matter.Body.setPosition(bird.body, {x: 200, y: 50});
+
     }
 }
 
